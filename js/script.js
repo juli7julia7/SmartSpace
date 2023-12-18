d5c2625e2899792490334ebe9cc78f0991e9ace8
juli7julia7 <juli7julia7@gmail.com>
2023-12-18 22:21:37 +0200

    add section hero

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.slider-hero ', {
+    // Optional parameters
+    loop: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  
+  });
