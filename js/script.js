6afa42e9b5461e713bd066716036f759413c0ccb
juli7julia7 <juli7julia7@gmail.com>
2024-03-29 14:36:22 +0200

    add menu burger

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
+
+'use script';
+
 const swiper = new Swiper('.slider-hero ', {
-    // Optional parameters
-    loop: true,
-  
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  
-  });
+  // Optional parameters
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
+
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+  const targetItem = e.target;
+
+  if (targetItem.closest('.icon-menu')) {
+    document.documentElement.classList.toggle('menu-open')
+  }
+}
+
