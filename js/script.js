b6a094a7fc312af1d983e7ac08f97ee8ebeb50f6
juli7julia7 <juli7julia7@gmail.com>
2024-12-20 21:55:27 +0200

    update

--- js/script.js
+++ js/script.js
@@ -22,7 +22,8 @@ function documentClick(e) {
   const targetItem = e.target;
 
   if (targetItem.closest('.icon-menu')) {
-    document.documentElement.classList.toggle('menu-open')
+    document.documentElement.classList.toggle('menu-open');
+    document.querySelector('html').classList.toggle('scroll-off');
   }
 }
 
